da40d175aca64eb0d7488822d78ea37cf3a9ad92
Raul Guzman <rguzmanc@fscm3p2mwz1.sephoraus.com>
2024-06-03 15:41:23 -0500

    Feature: Add initial structure for cmi ios project

---
+++ cmi/cmi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cmi
+//
+//  Created by Raul Guzman on 3/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+
