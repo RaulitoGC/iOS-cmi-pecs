44a76e5ea936c76bc7761740821883dcfcede1ea
Raul Guzman <rguzmanc@Rauls-Mac-Studio.local>
2024-06-06 09:30:46 -0500

    Feature: Add welcome Screen

---
+++ cmi/cmi/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  cmi
+//
+//  Created by Raul Guzman on 6/06/24.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ cmi/cmi/extension/String+Localized.swift
@@ -0,0 +1,13 @@
+//
+//  String+Localized.swift
+//  cmi
+//
+//  Created by Raul Guzman on 6/06/24.
+//
+
+import Foundation
+
+extension String {
+    
+
+}
